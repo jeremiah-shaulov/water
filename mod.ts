c22fbd19847d464689ecf60487a717e49131f29b
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2024-01-04 12:21:39 +0200

    Initial

---
+++ mod.ts
@@ -0,0 +1,3 @@
+export {RdStream, type Source} from './private/rd_stream.ts';
+export {WrStream, type Sink} from './private/wr_stream.ts';
+export {TrStream, type Transformer} from './private/tr_stream.ts';
